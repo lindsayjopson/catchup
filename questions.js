000fdf9967d52da0d2084b8b79d4f9a0522401e6
Lindsay Jopson <lindsayj@Lindsays-MacBook-Pro.local>
2018-07-11 09:01:00 +1200

    moar questions! 📋

--- questions.js
+++ questions.js
@@ -1,26 +1,95 @@
 export default{
   "questions": [
     {"ShortTerm": [
+      "How is [project] going? What could we do to make it better?",
       "Is there anything blocking you from getting your work done?",
-      "What parts of your job would you like to deepen your skills in or get additional training in?"
+      "Are there any projects you’d really like to work on if you were given the opportunity?",
+      "What parts of your job would you like to deepen your skills in or get additional training in?",
+      "Is any part of your project unclear or confusing?"
+    ]},
+    {"LongTerm": [
+      "What do you want to be doing in 5 years? 10 years? 3 years?",
+      "What are your long term goals? Have you thought about them?",
+      "Do you feel like you’re making progress on your big goals here? Why or why not?",
+      "What’s one thing we could do today to help you with your long term goals?",
+      "Do you feel we’re helping you advance your career at a pace you would like?",
+      "If you had millions of dollars, what would you do every day?",
+      "What are your super powers? What powers would you like to develop?",
+      "What are your big dreams in life? Are you making progress on them?",
+      "Could you see yourself making progress on more of your goals here? What would need to change to do so?",
+      "What work are you doing here that you feel is most in line with your long term goals?",
+      "As a kid, what did you want to be when you grew up?"
     ]},
     {"SelfImprovement" : [
+      "Do you feel challenged at work? Are you learning new things?",
+      "What area of the company would you like to learn more about?",
       "What skills would you like to develop right now?",
-      "How many hours a day do you feel you’re productive? How could we help you be more productive?"
+      "Who in the company would you like to learn from? What do you want to learn?",
+      "How do you prefer to receive feedback?",
+      "Do you feel you’re getting enough feedback?",
+      "What’s a recent situation you wish you handled differently? What would you change?",
+      "What additional training or education would you like?",
+      "Are there any roles in the company you’d like to learn more about?",
+      "What do you think are the key skills for your role? How would you rate yourself for each of them?",
+      "Is there an aspect of your job you would like more help or coaching?"
     ]},
     {"BusinessGoals" : [
+      "What is the company not doing today that we should do to better compete in the market?",
+      "What’s one thing we’d be *crazy* not to do in the next quarter to improve our product?",
       "How could we change our team meetings to be more effective?",
-      "How many hours a day do you feel you’re productive? How could we help you be more productive?"
+      "If you were CEO, what’s the first thing you’d change?",
+      "Do you think our company is loyal to its employees? Why or why not?",
+      "Are there any aspects of our culture you wish you could change?",
+      "What are your favorite parts about our culture?",
+      "Do you feel over-worked, under-worked, or just the right workload?",
+      "Why do you think [employee who recently quit] left? What did they tell you?",
+      "What would convince you to leave for a job somewhere else?",
+      "Which company values do you like the most? Which the least? Why?",
+      "What is the #1 Problem at our company? Why?",
+      "Do you feel like you’re on the same page with your team? How often do you think you need meetings to ensure you stay that way?",
+      "What do you think are the long term prospects of the company?",
+      "How many hours a day do you feel you’re productive? How could we help you be more productive?",
+      "How could we be more creative or innovative as a company?"
+    ]},
+    {"ManagerImprovement" : [
+      "What could I do as a manager to make your work easier?",
+      "What do you like about my management style? What do you dislike?",
+      "Would you like more or less direction from me on your work?",
+      "What could I do to make you enjoy your work more?",
+      "How can I better support you?",
+      "What would you like to know about me?",
+      "Is there a situation you’d like my help with?",
+      "What is something I could do better? What is a criticism you have for me?"
     ]},
     {"Happiness" : [
-      "What part of your job do you wish you didn’t have to do?",
-      "Are you happy with your recent work? Why or why not?"
+      "Are you happy working here?",
+      "Are you happy with your recent work? Why or why not?",
+      "What would make you leave this job for another?",
+      "What’s one thing we do to help you enjoy your job more?",
+      "Is your job what you expected when you accepted it?",
+      "What worries you?",
+      "What’s on your mind?",
+      "What’s not fun about working here? What do you enjoy most about working here?",
+      "When was the time you enjoyed working here the most?",
+      "What do you feel is your greatest accomplishment here?",
+      "What’s something you feel is undervalued that you contribute to the team?",
+      "What part of your job do you wish you didn’t have to do?"
     ]},
     {"TeamRelations" : [
+      "Who on the team do you have the most difficulty working with? Why?",
       "How would you describe the work environment on the team? Is it more competitive or collaborative?",
+      "How could we improve the ways our team works together?",
       "Who is kicking ass on the team? What have they done?",
+      "Who do you admire on the team? Why?",
+      "Do you feel your ideas are heard by the team and I?",
+      "Who would you like to work more often with? Why?",
       "Is everyone pulling their weight on the team?",
-      "What’s the biggest thing you’d like to change about our team?"
+      "Do you help other members on the team? Do others help you when you need it?",
+      "What’s one thing we should change about how our team works together?",
+      "What characteristics make someone a good fit for our team? How would you look for those characteristics in an interview?",
+      "What’s the biggest thing you’d like to change about our team?",
+      "What do you like most about working on our team?",
+      "Has anyone on the team ever made you feel uncomfortable? What happened?"
     ]}
   ]
 }
