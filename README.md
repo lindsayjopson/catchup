750c7b8be7bcc836a4578e65a64998455fc9d4d2
Lindsay Jopson <lindsayjopson@gmail.com>
2020-02-17 09:51:44 +1300

    adding VGraupera

--- README.md
+++ README.md
@@ -9,25 +9,31 @@ This has been created as a question prompt for leaders to ask during regular cat
 They are set up with a bunch different areas of focus. Ranging from Self Improvement & Happiness to Long Term business & Management improvement questioning.
 
 ##### Questions contributors:
-  * [Jason Evanish](https://twitter.com/evanish) post [101 Questions to Ask in One on Ones](https://jasonevanish.com/2014/05/29/101-questions-to-ask-in-1-on-1s/)
-  * [Movio](http://www.movio.co)
-  * [Stripe engineering blog](https://stripe.com/atlas/guides/scaling-eng)
+
+- [Jason Evanish](https://twitter.com/evanish) post [101 Questions to Ask in One on Ones](https://jasonevanish.com/2014/05/29/101-questions-to-ask-in-1-on-1s/)
+- [Movio](http://www.movio.co)
+- [Stripe engineering blog](https://stripe.com/atlas/guides/scaling-eng)
+- [VGraupera](https://github.com/VGraupera/1on1-questions)
 
 #### Adding questions
-Adding questions is totally extendible by editing the ```questions.js``` file.
+
+Adding questions is totally extendible by editing the `questions.js` file.
 Alternatively open a PR into this repo and I will be more then happy to merge!
 
 ### Installation
+
 ```
 npm install
 ```
 
 #### To build
+
 ```
 npm run build
 ```
 
 #### Run on local machine
+
 ```
 npm start
 ```
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import dataSet from '../questions';
+import dataSet from "../questions";
 import "../style.css";
 
 function getRandomInt(max) {
@@ -6,71 +6,62 @@ function getRandomInt(max) {
 }
 
 const getQuestionDetail = () => {
-  const randGroup = dataSet.questions[
-    getRandomInt(dataSet.questions.length)
-  ];
+  const randGroup = dataSet.questions[getRandomInt(dataSet.questions.length)];
 
   const randGroupSet = Object.values(randGroup)[0];
-  const randQuestion = randGroupSet[
-    getRandomInt(randGroupSet.length)
-  ];
+  const randQuestion = randGroupSet[getRandomInt(randGroupSet.length)];
 
   return renderQuestion(randGroup, randQuestion);
-}
+};
 
 const renderQuestion = (questionHeading, questionText) => (
-  <div className='question_wrap'>
-    <div className='padding question-text'>
-      {questionText}
-    </div>
-    <span className='padding question-heading'>
-      {Object.keys(questionHeading).toString().replace(/([a-z](?=[A-Z]))/g, '$1 ')}
+  <div className="question_wrap">
+    <div className="padding question-text">{questionText}</div>
+    <span className="padding question-heading">
+      {Object.keys(questionHeading)
+        .toString()
+        .replace(/([a-z](?=[A-Z]))/g, "$1 ")}
     </span>
   </div>
 );
 
 function navClick() {
-    const slides = document.getElementsByClassName("slide");
+  const slides = document.getElementsByClassName("slide");
 
-    for(var i=0; i < slides.length; i++) {
-        if(slides[i].classList.contains('active')){
-            slides[i].classList.remove('active');
-            if(i+1 >= slides.length){
-                slides[0].classList.add('active');
-                break;
-            }else {
-                slides[i+1].classList.add('active');
-                break;
-            }
-        }
+  for (var i = 0; i < slides.length; i++) {
+    if (slides[i].classList.contains("active")) {
+      slides[i].classList.remove("active");
+      if (i + 1 >= slides.length) {
+        slides[0].classList.add("active");
+        break;
+      } else {
+        slides[i + 1].classList.add("active");
+        break;
+      }
     }
+  }
 }
 
 const renderNav = () => (
-    <button
-       className="btn"
-       onClick={navClick}
-    >
-        Next
-    </button>
+  <button className="btn" onClick={navClick}>
+    Next
+  </button>
 );
 
 export default () => (
   <div>
-    <ul className='slide_wrap'>
-      <li className='slide active'>
-          {getQuestionDetail()}
-      </li>
-      <li className='slide'>
-          {getQuestionDetail()}
-      </li>
-      <li className='slide'>
-          {getQuestionDetail()}
-      </li>
+    <ul className="slide_wrap">
+      <li className="slide active">{getQuestionDetail()}</li>
+      <li className="slide">{getQuestionDetail()}</li>
+      <li className="slide">{getQuestionDetail()}</li>
     </ul>
-        {renderNav()}
-        <a class="logo--github" href="https://github.com/lindsayjopson/catchup" target="_blank">
-        lindsayjopson <img src="./static/github.png" /> 
-        </a>
+    {renderNav()}
+    <a
+      className="logo--github"
+      href="https://github.com/lindsayjopson/catchup"
+      target="_blank"
+    >
+      lindsayjopson <img src="./static/github.png" />
+    </a>
   </div>
 );
